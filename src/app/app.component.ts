29f1c409974e9b4788c85c1dcd83993484510a89
Angel Sanchez <dual.angel.sanchez@gmail.com>
2022-12-27 10:08:43 -0600

    Reto del consumo de Api se muestran por consola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
+import { state } from '@angular/animations';
 import { Component } from '@angular/core';
+import { Store, Action } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
+import { PokemonService } from './service/pokemon.service';
+
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'redux-app';
+
+  contador!: number;
+
+  name!: string;
+  urlImage: any;
+
+  constructor(private store: Store<AppState>, private pokemonService : PokemonService) {
+    this.store.subscribe(state=> {
+      this.contador = state.contador;
+      this.urlImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
+      //console.log(state);
+    })
+    ngOnInit(); void {
+    }
+  }
+
+  incrementar(){
+    this.contador ++;
+
+   const accion = new IncrementarAction();
+
+   this.pokemonService.getPokemonPorId(this.contador).subscribe((data:any)=> {
+    this.urlImage = data.sprites.front_default
+    this.name = data.name;
+
+    console.log(data);
+
+  });
+
+   this.store.dispatch (accion);
+  }
+
+  decrementar(){
+   this.contador --;
+    const accion = new DecrementarAction();
+    this.pokemonService.getPokemonPorId(this.contador).subscribe((data:any)=> {
+      this.urlImage = data.sprites.front_default
+      this.name = data.name;
+      console.log(data);
+    });
+
+   this.store.dispatch (accion);
+  }
+
+  ngOnInit(): void {
+  }
 }
+function ngOnInit() {
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { contadorReducer } from './contador/contador.reducer';
+import { count } from 'rxjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environment/environment';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HijoComponent,
+    NietoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot( {contador: contadorReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
